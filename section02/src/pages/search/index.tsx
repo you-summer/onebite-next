9cc8f8e38c70bd83937e6bb1e11654a4dd940b10
(조연정) <jojoor201@gmail.com>
2026-01-24 18:42:17 +0900

    day5

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,15 +1,17 @@
 import SearchableLayout from "@/components/serachable-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-  const router = useRouter();
-
-  console.log(router);
-
-  const { q } = router.query;
-
-  return <h1>Search {q}</h1>;
+  return (
+    <div>
+      {books.map((book) => {
+        return <BookItem key={book.id} {...book} />;
+      })}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
