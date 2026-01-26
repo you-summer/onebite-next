7b069627344421dd20711480dd126cec267cb433
(조연정) <jojoor201@gmail.com>
2026-01-26 20:18:09 +0900

    day6

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,5 +1,10 @@
 import { useRouter } from "next/router";
 import style from "./[id].module.css";
+import {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
+import fetchOneBook from "@/lib/fetch-one-book";
 
 const mockData = {
   id: 1,
@@ -13,7 +18,24 @@ const mockData = {
     "https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg",
 };
 
-export default function Page() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
+  const id = context.params!.id;
+  const book = await fetchOneBook(Number(id));
+  console.log(id);
+  return {
+    props: {
+      book,
+    },
+  };
+};
+
+export default function Page({
+  book,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!book) return "문제가 발생했습니다 다시 시도하세요";
+
   const {
     id,
     title,
@@ -22,7 +44,7 @@ export default function Page() {
     author,
     publisher,
     coverImgUrl,
-  } = mockData;
+  } = book;
 
   return (
     <div className={style.container}>
