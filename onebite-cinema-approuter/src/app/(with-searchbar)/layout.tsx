2eda799131d66b7505a19d56862eaa17d2d61ffd
조연정 <jojoor201@gmail.com>
2026-02-03 19:01:22 +0900

    day9+HW

---
+++ onebite-cinema-approuter/src/app/(with-searchbar)/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <div>Searchbar Layout</div>
+      {children}
+    </div>
+  );
+}
