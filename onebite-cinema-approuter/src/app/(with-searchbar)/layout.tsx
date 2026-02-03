ee2a49d4628c6f71399e4045c97b7716b9571e38
조연정 <jojoor201@gmail.com>
2026-02-03 23:49:52 +0900

    HW-day10-1

--- onebite-cinema-approuter/src/app/(with-searchbar)/layout.tsx
+++ onebite-cinema-approuter/src/app/(with-searchbar)/layout.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from "react";
+import Searchbar from "./searchbar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
       <div>Searchbar Layout</div>
+      <Searchbar />
       {children}
     </div>
   );
