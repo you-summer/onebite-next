5a31665a965d4e7a594567aa75b414d27aa91479
조연정 <jojoor201@gmail.com>
2026-02-05 16:54:28 +0900

    HW day10

--- onebite-cinema-approuter/src/app/(with-searchbar)/searchbar.tsx
+++ onebite-cinema-approuter/src/app/(with-searchbar)/searchbar.tsx
@@ -1,17 +1,35 @@
 "use client";
 
-import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useState } from "react";
 
 export default function Searchbar() {
-  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+
+  const query = searchParams.get("q") ?? "";
+
+  const [search, setSearch] = useState(query);
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
+  const onSubmit = () => {
+    if (!search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
   return (
     <div>
-      <input value={search} onChange={onChangeSearch} />
-      <button>검색</button>
+      <input value={search} onChange={onChangeSearch} onKeyDown={onKeyDown} />
+      <button onClick={onSubmit}>검색</button>
     </div>
   );
 }
