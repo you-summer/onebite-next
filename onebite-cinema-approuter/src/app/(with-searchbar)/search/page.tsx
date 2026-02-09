ef30c85b7e7d064ae57a4474b03fec34a9dd440c
조연정 <jojoor201@gmail.com>
2026-02-09 17:16:57 +0900

    HWday14

--- onebite-cinema-approuter/src/app/(with-searchbar)/search/page.tsx
+++ onebite-cinema-approuter/src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,7 @@
 import MovieItem from "@/components/movie-item";
 import style from "./page.module.css";
 import { MovieData } from "@/types";
+import { delay } from "@/util/delay";
 
 export default async function Page({
   searchParams,
@@ -8,6 +9,8 @@ export default async function Page({
   searchParams: Promise<{ q?: string }>;
 }) {
   const { q } = await searchParams;
+
+  await delay(1500);
 
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${q}`,
