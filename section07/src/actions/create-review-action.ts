afba2a3addde61cf1b2b0716b31fe314f6d0a0e1
조연정 <jojoor201@gmail.com>
2026-02-10 23:38:04 +0900

    day17-1

--- section07/src/actions/create-review-action.ts
+++ section07/src/actions/create-review-action.ts
@@ -1,6 +1,8 @@
 "use server";
 
-export async function createReviewAction(formData: FormData) {
+import { revalidatePath, revalidateTag } from "next/cache";
+
+export async function createReviewAction(_: any, formData: FormData) {
   // console.log("server action cold");
   // console.log(formData);
   const bookId = formData.get("bookId")?.toString();
@@ -10,7 +12,10 @@ export async function createReviewAction(formData: FormData) {
   //   console.log(bookId, content, author);
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요",
+    };
   }
   try {
     const response = await fetch(
@@ -21,8 +26,34 @@ export async function createReviewAction(formData: FormData) {
       },
     );
     console.log(response.status);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // 1. 특정 주소의 해당하는 페이지만 재검증
+    // revalidatePath(`/book/${bookId}`); // 넥스트 서버가 자동으로 재검증(다시생성)하게됨
+    // // 오직 서버측에서만 쓸수있음
+
+    // // 2. 특정 경로의 모든 동작 페이지를 재검증
+    // revalidatePath("/book/[id]", "page");
+
+    // // 3. 특정 레이아웃을 갖는 모든 페이지 재검증
+    // revalidatePath("/(with-searchbar)", "layout");
+
+    // // 4. 모든 데이터 재검증
+    // revalidatePath("/", "layout");
+
+    // 5. 태그 기준, 데이터 캐시 재검증
+    revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    // console.error(err);
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${err}`,
+    };
   }
 }
