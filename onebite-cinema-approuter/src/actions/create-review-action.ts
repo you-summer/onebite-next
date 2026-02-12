6a091c3b59e4f7ba147fa1c446f7e6d0a0c84e22
조연정 <jojoor201@gmail.com>
2026-02-12 18:09:15 +0900

    day17

--- onebite-cinema-approuter/src/actions/create-review-action.ts
+++ onebite-cinema-approuter/src/actions/create-review-action.ts
@@ -1,12 +1,17 @@
 "use server";
 
-export async function createReviewAction(formData: FormData) {
+import { revalidateTag } from "next/cache";
+
+export async function createReviewAction(_: any, formData: FormData) {
   const movieId = formData.get("movieId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
   if (!movieId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요",
+    };
   }
 
   try {
@@ -18,8 +23,20 @@ export async function createReviewAction(formData: FormData) {
       },
     );
     console.log(response.statusText);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    revalidateTag(`review-${movieId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    // console.error(err);
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${err}`,
+    };
   }
 }
--- onebite-cinema-approuter/src/components/review-editor.tsx
+++ onebite-cinema-approuter/src/components/review-editor.tsx
@@ -1,15 +1,41 @@
-import { createReviewAction } from "@/actions/create-review-action";
+"use client";
+
+import { useActionState, useEffect } from "react";
 import style from "./review-editor.module.css";
+import { createReviewAction } from "@/actions/create-review-action";
 
 export default function ReviewEditor({ movieId }: { movieId: string }) {
+  const [state, formAction, isPending] = useActionState(
+    createReviewAction,
+    null,
+  );
+
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+
   return (
     <section>
-      <form action={createReviewAction} className={style.form_container}>
+      <form action={formAction} className={style.form_container}>
         <input name="movieId" value={movieId} hidden readOnly />
-        <textarea required name="content" placeholder="리뷰 내용" />
+        <textarea
+          disabled={isPending}
+          required
+          name="content"
+          placeholder="리뷰 내용"
+        />
         <div className={style.submit_container}>
-          <input required name="author" placeholder="작성자" />
-          <button type="submit">작성하기</button>
+          <input
+            disabled={isPending}
+            required
+            name="author"
+            placeholder="작성자"
+          />
+          <button disabled={isPending} type="submit">
+            {isPending ? "..." : "작성하기"}
+          </button>
         </div>
       </form>
     </section>
